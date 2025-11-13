423044bfc5f14d411914bda6050c17f02524172c
Nuntius Creative Hub <nuntius.creative.hub@gmail.com>
2025-11-13 09:43:22 -0300

    Refactor entrypoint.sh comments and cron execution
    Updated comments for clarity and changed cron execution to run in the background. Added a note about keeping the container alive.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Crear directorio de logs si no existe
+# Crear directorio de logs y el archivo de log vacío
 mkdir -p /app/logs
+touch /app/logs/cron.log
 
 # Imprimir zona horaria configurada
 echo "Zona horaria configurada: $(date)"
 echo "Iniciando servicio de descarga automática ANSV..."
 echo "El script se ejecutará todos los días a las 6:00 AM"
 
-# Iniciar cron en foreground
+# Iniciar cron en segundo plano
 echo "Iniciando cron..."
-cron && tail -f /app/logs/cron.log
+cron
+
+# Iniciar 'tail' en primer plano.
+# Esto es lo que mantendrá el contenedor vivo.
+# Ahora tendrá éxito porque el archivo 'cron.log' ya existe.
+tail -f /app/logs/cron.log
